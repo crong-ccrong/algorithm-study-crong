0739a427376eb065e43954f4790b3d31df474ac2
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2026-03-20 18:23:57 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.07 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        
+        for (int i = left; i <= right; i++) {
+            answer = i % Math.sqrt(i) == 0 ? answer - i : answer + i;
+        }
+        
+        return answer;
+    }
+}
