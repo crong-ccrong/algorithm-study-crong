d86422497ff055c13df301dd7c328b39a35a262a
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2026-09-11 09:46:16 +0900

    [level 0] Title: OX퀴즈, Time: 0.15 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] quiz) {
+        ArrayList<String> answer = new ArrayList<>();
+        
+        for (String str : quiz) {
+            String[] s = str.split(" ");
+            int X = Integer.parseInt(s[0]);
+            int Y = Integer.parseInt(s[2]);
+            String operator = s[1];
+            int Z = Integer.parseInt(s[4]);
+
+            switch(operator) {
+                case "+" :
+                    answer.add(X + Y == Z ? "O" : "X");
+                    break;
+                case "-" :
+                    answer.add(X - Y == Z ? "O" : "X");
+            }
+        }
+        
+        return answer.toArray(new String[0]);
+    }
+}
